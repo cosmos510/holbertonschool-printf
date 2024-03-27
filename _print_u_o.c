6b699677afdc201549637ce60929f78f801312cc
cosmos510 <maximemartin510@gmail.com>
2024-03-27 15:00:33 +0000

    add _print ui and print_o

---
+++ _print_u_o.c
@@ -0,0 +1,54 @@
+#include"main.h"
+int print_ui(va_list *ptr)
+{
+	int count = 0;
+	unsigned int i = va_arg(*ptr, unsigned int);
+
+	help_ui(i, &count);
+	return (count);
+}
+/**
+ * help_ui - This function is a helper for print_ui. It prints the
+ *           unsigned integer i recursively. It updates the count of
+ *           characters printed through the pointer pcount.
+ * @i: Unsigned integer to be printed.
+ * @pcount: Pointer to the count of characters printed.
+ */
+void help_ui(unsigned int i, int *pcount)
+{
+	if (i / 10 != 0)
+	{
+		help_ui(i / 10, pcount);
+	}
+	_putchar(i % 10 + '0');
+	(*pcount)++;
+}
+/**
+ * print_o - Prints octal numbers from the list of arguments.
+ * @ptr: Pointer to the list of arguments.
+ * Return: the count of characters printed.
+ */
+int print_o(va_list *ptr)
+{
+	int count = 0;
+	unsigned int i = va_arg(*ptr, unsigned int);
+
+	help_o(i, &count);
+	return (count);
+}
+/**
+ * help_o - Helper function for printing octal numbers. It prints
+ *          the octal number i recursively. It updates the count of
+ *          characters printed through the pointer pcount.
+ * @i: Octal number to be printed.
+ * @pcount: Pointer to the count of characters printed.
+ */
+void help_o(unsigned int i, int *pcount)
+{
+	if (i != 0)
+	{
+		help_o(i / 8, pcount);
+		putchar((i % 8) + '0');
+		(*pcount)++;
+	}
+}
